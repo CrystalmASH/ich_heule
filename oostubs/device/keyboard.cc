f1ef64a8566d9a06ef19ffd509d0570a5ac0ba79
CrystalmASH <news30@gmx.net>
2025-05-06 23:11:23 +0200

    correct some stuff and keyboard

--- oostubs/device/keyboard.cc
+++ oostubs/device/keyboard.cc
@@ -10,12 +10,16 @@
 #include "device/keyboard.h"
 #include "device/cgastr.h"
 /* Add your code here */ 
-Keyboard::Keyboard(){}
+Keyboard::Keyboard(	PIC* pi, Plugbox* plug){
+    pic = pi;
+    plugbox = plug;
+}
 /* Add your code here */ 
 void Keyboard::trigger(){
+    CGA_Stream kout;
     if (inb(0x64) & 0b1 == 1)
     {
-        CGA_Stream kout;
+        
         Key pressed_key = Keyboard::key_hit();
          if(pressed_key.valid())
             if( pressed_key.scancode() == Key::scan::del and pressed_key.alt() and pressed_key.ctrl()){
@@ -24,6 +28,7 @@ void Keyboard::trigger(){
         {
             unsigned char character = pressed_key.ascii();
             if (character != 0){
+                kout.setpos(1,1);
                 kout << character;
             }
             
@@ -33,5 +38,6 @@ void Keyboard::trigger(){
 }
 
 void Keyboard::plugin(){
-
+    pic->allow(1);
+    plugbox->assign(33, *this);
 }
--- oostubs/device/keyboard.h
+++ oostubs/device/keyboard.h
@@ -14,6 +14,8 @@
 #include "machine/keyctrl.h"
 #include "guard/gate.h"
 #include "machine/key.h"
+#include "machine/plugbox.h"
+#include "machine/pic.h"
  
 class Keyboard  : public Keyboard_Controller, public Gate
 /* Add your code here */ 
@@ -21,9 +23,11 @@ class Keyboard  : public Keyboard_Controller, public Gate
 public:
 	Keyboard(const Keyboard &copy) = delete; // prevent copying
 	Keyboard& operator=(const Keyboard&) = delete; // prevent assignment
+	PIC* pic;
+	Plugbox* plugbox;
 
 /* Add your code here */ 
-	Keyboard();
+	Keyboard(PIC* pi, Plugbox* plug);
 /* Add your code here */ 
  
 	// PLUGIN: "Plugs in" the keyboard (driver). From now on, keypresses are handled.
